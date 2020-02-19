b7296e8ad49487245b90520312fa9ad5b6debf54
Enrique Gonzalez <enrique@saucelabs.com>
2020-02-18 16:13:41 -0800

    make loop 5 times

--- tests/loop-web.ts
+++ tests/loop-web.ts
@@ -1,6 +1,6 @@
 describe("Google Test", () => {
     it("should go to google and click Sauce Labs", () => {
-        for (let index = 0; index < 20; index++) {
+        for (let index = 0; index < 5; index++) {
             
             browser.url("https://google.com");
     
